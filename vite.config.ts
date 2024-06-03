18916a81509a0bf7067d64d272efa277f7ffe4b4
52HZMercury <chenning_william@163.com>
2024-06-03 09:56:34 +0800

    Gaolileo

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
+import path from 'path'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), UnoCSS(),],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
